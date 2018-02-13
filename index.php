f49210b2ff1165dae64dd86dc271c2a2635714b0
Git² <GitSquared@users.noreply.github.com>
2018-02-13 19:45:02 +0100

    Use only localhost in login servers

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ function adminer_object()
     $plugins = [
         new AdminerDatabaseHide(["mysql", "information_schema", "performance_schema"]),
         new AdminerLoginServers([
-            filter_input(INPUT_SERVER, 'HTTP_HOST') => filter_input(INPUT_SERVER, 'SERVER_NAME')
+            "localhost"
         ]),
         new AdminerSimpleMenu(),
         new AdminerCollations(),
