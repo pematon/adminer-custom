724982f51742f97774253a718a4e39eed8423e9c
Peter Knut <peter@pematon.com>
2019-07-28 21:30:23 +0200

    Change distribution script to reflect moved sources

--- bin/distribute.sh
+++ bin/distribute.sh
@@ -10,15 +10,16 @@ cd "$BASEDIR"
 cp index.php ../adminer-custom
 cp adminer.php ../adminer-custom
 
-cp -r css ../adminer-custom
+cp -r css/*.css ../adminer-custom/css
 cp -r fonts ../adminer-custom
 cp -r images ../adminer-custom
 cp -r plugins ../adminer-custom
 
 # Theme
-cp -r less/* ../adminer-theme/src
+cp -r css/*.less ../adminer-theme/src
+cp -r css/inc ../adminer-theme/src
 
-cp -r css ../adminer-theme/lib
+cp -r css/*.css ../adminer-theme/lib/css
 cp -r fonts ../adminer-theme/lib
 cp -r images ../adminer-theme/lib
 cp plugins/AdminerTheme.php ../adminer-theme/lib/plugins
